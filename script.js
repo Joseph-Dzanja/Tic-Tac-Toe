9166263d638baee315603907ee46256498a19f73
Joseph Dzanja <jhdzanja@gmail.com>
2024-07-07 19:48:34 +0200

    refactored the javascript to work with UI

--- script.js
+++ script.js
@@ -25,64 +25,115 @@ let game = () =>{
         return { playerName, playerTag, playerSymbol };
     }
 
-    function turn(){
-        let turn = 0;
-        let count = 0;
-        for(let i = 0;i==0;i){
-            
-            if(turn == 0){
-                console.log(`${player1.playerTag}s turn`);
-                let p1 = prompt(`Select your square ${player1.playerName}`);
-                let first = Number(p1[0]);
-                let second = Number(p1[1]);
-                if(board[first][second]==='X' || board[first][second] === 'O'){
-                    console.log("Already marked!");
-                    turn = 0;
-                    continue;
-                }
+    let turnNum = 0;
+    let count = 0;
+    function turn(value){
+    
+        if(turnNum == 0){
+            // console.log(`${player1.playerTag}s turn`);
+            // let p1 = prompt(`Select your square ${player1.playerName}`);
+            let first = Number(value[0]);
+            let second = Number(value[1]);
+            if(board[first][second]==='X' || board[first][second] === 'O'){
+                console.log("Already marked!");
+                turnNum = 0;
+                
+            }
+            else{
                 board[first][second] = player1.playerSymbol;
-                //
-                turn = 1;
+                console.log(`.t${value}`)
+                document.querySelector(`.t${value} .char`).innerHTML = player1.playerSymbol;
+                turnNum = 1;
             }
-            else if(turn == 1){
-                console.log(`${player2.playerTag}s turn`);
-                let p2 = prompt(`Select your square ${player2.playerName}`);
-                let first = Number(p2[0]);
-                let second = Number(p2[1]);
-                if(board[first][second]==='X' || board[first][second] === 'O'){
-                    console.log("Already marked!");
-                    turn = 1;
-                    continue;
-                }
-                board[first][second] = player2.playerSymbol;
-                //
-                turn = 0;
-            }
-            count++;
             
-            
-            if(count > 3 && count < 9){
-                if (checkForWinner()===1){
-                    console.log(`${player1.playerName} wins`);
-                    newgame.displayboard();
-                    break;
-                }
-                else if(checkForWinner()===2){
-                    console.log(`${player2.playerName} wins`);
-                    newgame.displayboard();
-                    break;
-                }
-
-            }
-            else if(count > 8){
-                console.log("its a draw!");
-                newgame.displayboard();
-                break;
-            }
-            newgame.displayboard();
         }
-       
-    }
+        else if(turnNum == 1){
+            // console.log(`${player2.playerTag}s turn`);
+            // let p2 = prompt(`Select your square ${player2.playerName}`);
+            let first = Number(value[0]);
+            let second = Number(value[1]);
+            if(board[first][second]==='X' || board[first][second] === 'O'){
+                console.log("Already marked!");
+                turnNum = 1;
+                
+            }
+            else{
+                board[first][second] = player2.playerSymbol;
+                console.log(`.t${value}`)
+                document.querySelector(`.t${value} .char`).innerHTML = player2.playerSymbol;
+                turnNum = 0;
+            }
+        }
+
+        
+        count++;
+        
+        console.log(count);
+        if(count > 3 && count < 9){
+            if (checkForWinner()===1){
+                console.log(`${player1.playerName} wins`);
+                newgame.displayboard();
+                result.innerText = 'player X wins!';
+                turnNum = 3;
+                
+            }
+            else if(checkForWinner()===2){
+                console.log(`${player2.playerName} wins`);
+                newgame.displayboard();
+                result.innerText = 'player O wins!';
+                turnNum = 3;
+            }
+
+        }
+        else if(count > 8){
+            console.log("its a draw!");
+            newgame.displayboard();
+            result.innerText = 'Its a draw!';
+            turnNum = 3;
+        }
+        newgame.displayboard();
+    
+   
+}
+
+let result = document.getElementById('result');
+let t00 = document.querySelector('.t00');
+let t01 = document.querySelector('.t01');
+let t02 = document.querySelector('.t02');
+let t10 = document.querySelector('.t10');
+let t11 = document.querySelector('.t11');
+let t12 = document.querySelector('.t12');
+let t20 = document.querySelector('.t20');
+let t21 = document.querySelector('.t21');
+let t22 = document.querySelector('.t22');
+
+t00.addEventListener('click', () => {
+    turn('00');
+})
+t01.addEventListener('click', () => {
+    turn('01')
+})
+t02.addEventListener('click', () => {
+    turn('02')
+})
+t10.addEventListener('click', () => {
+    turn('10')
+})
+t11.addEventListener('click', () => {
+    turn('11')
+})
+t12.addEventListener('click', () => {
+    turn('12')
+})
+t20.addEventListener('click', () => {
+    turn('20')
+})
+t21.addEventListener('click', () => {
+    turn('21')
+})
+t22.addEventListener('click', () => {
+    turn('22')
+})
 
     let checkForWinner = () =>{
         
@@ -131,6 +182,6 @@ let game = () =>{
 const newgame = game();
 newgame.newBoard();
 newgame.displayboard();
-const player1 = newgame.newPlayer(prompt("input name player 1"),1);
-const player2 = newgame.newPlayer(prompt("input name player 2"),2);
-newgame.turn();
+const player1 = newgame.newPlayer('player X',1);
+const player2 = newgame.newPlayer('player O',2);
+
